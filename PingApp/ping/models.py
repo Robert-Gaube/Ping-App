74b05b890d1456c3e4d850b653e89fb03e9dd5c3
Gaube <robert.gabriel.gaube@intel.com>
2023-02-14 11:18:10 +0200

    Adding validators and str

--- PingApp/ping/models.py
+++ PingApp/ping/models.py
@@ -1,5 +1,24 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
+def validate_ip(ip):
+     for char in ip:
+          if char.isalpha():
+               raise ValidationError(
+                    _('%(ip)s contains letters'),
+                    params={'ip':ip},
+               )
+          if char == ' ':
+                raise ValidationError(
+                    _('%(ip)s contains spaces'),
+                    params={'ip':ip},
+               )
+     
 class Setup(models.Model):
      name = models.CharField(max_length=50)
-     ip = models.CharField(max_length=30)
+     ip = models.CharField(max_length=30, validators=[validate_ip])
+
+     def __str__(self):
+          return f'{self.name} - {self.ip}'
+          
--- PingApp/ping/views.py
+++ PingApp/ping/views.py
@@ -3,6 +3,7 @@ from django.template.loader import render_to_string
 from django.http import HttpResponse, HttpResponseNotFound
 import subprocess
 import platform
+from . import models
 
 def ping(ip):
     try:
